4d9c7980b206c9482d154add1320ec878fee50e0
alo40 <alejandronieto40@gmail.com>
2023-03-27 23:00:04 +0200

    added EEPROM example

---
+++ save_to_EEPROM/main_EEPROM.cpp
@@ -0,0 +1,59 @@
+void acquire_and_store_value_A()
+{
+    // Code for acquiring value from ADC port
+    int value = read_adc_port();
+    
+    // Code for storing value in EEPROM
+    write_to_eeprom(value);
+}
+
+void acquire_and_store_value_B()
+{
+    // Code for acquiring value from ADC port
+    int value = read_adc_port();
+    
+    // Code for checking and verifying the acquired value meets B requirements
+    if (value < B_MIN_VALUE || value > B_MAX_VALUE)
+    {
+        // Error handling code for value not meeting B requirements
+        handle_B_error();
+        return;
+    }
+    
+    // Code for storing value in EEPROM with B-compliant mechanisms
+    write_to_eeprom_with_check(value);
+}
+
+void acquire_and_store_value_C()
+{
+    // Code for acquiring value from ADC port
+    int value = read_adc_port();
+    
+    // Code for checking and verifying the acquired value meets C requirements
+    if (value < C_MIN_VALUE || value > C_MAX_VALUE)
+    {
+        // Error handling code for value not meeting C requirements
+        handle_C_error();
+        return;
+    }
+    
+    // Code for storing value in EEPROM with C-compliant mechanisms
+    write_to_eeprom_with_check_and_redundancy(value);
+}
+
+void acquire_and_store_value_D()
+{
+    // Code for acquiring value from ADC port
+    int value = read_adc_port();
+    
+    // Code for checking and verifying the acquired value meets  D requirements
+    if (value < D_MIN_VALUE || value > D_MAX_VALUE)
+    {
+        // Error handling code for value not meeting  D requirements
+        handle_D_error();
+        return;
+    }
+    
+    // Code for storing value in EEPROM with  D-compliant mechanisms
+    write_to_eeprom_with_check_redundancy_and_fault_detection(value);
+}
